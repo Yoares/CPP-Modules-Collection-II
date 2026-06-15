8d30c7183dba32aad622b74440971836e8358e90
Yoares <aresmanage33@gmail.com>
2026-06-16 00:58:38 +0100

    test

--- cpp-06/ex00/ScalarConverte.cpp
+++ cpp-06/ex00/ScalarConverte.cpp
@@ -31,27 +31,64 @@ ScalarConverte::~ScalarConverte()
 }
 
 bool isPseudo(std::string& s){
-    return false;
+    return (s == "nan" || s == "nanf" || s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff");
 }
 
 bool isChar(std::string& s){
-    return false;
+    return (s.size() == 1 && isprint(s[0]) && !isdigit(s[0]));
 }
 
 bool isFloat(std::string& s){
-    return false;
+    if (s.empty() || s[s.length() - 1] != 'f')  
+        return false;
+    if (s == "-inff" || s == "+inff" || s == "nanf") 
+        return true;
+    int = 0;
+    bool hasdigit = false;
+    bool hasdot = false;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (i < (int)s.length() - 1; i++){
+        if (isdigit(s[i]))
+            hasdigit = true;
+        else if (s[i] == '.' && !hasdot)
+            hasdot = true;
+        else
+            return false;
+    }
+    return hasdigit && hasdot;
 }
 
 bool isInt(std::string& s){
-    bool sign = false;
-    std::string sign_s;
-    std::string body;
-    
-    return false;
+    int i = 0;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    if (i == s.length()) 
+        return false;
+    for (i < (int)s.size(); i++){
+        if (!isdigit(s[i]))
+            return false;
+    }
+    return true;
 }
 
 bool isDouble(std::string& s){
-    return false;
+    if (s == "-inf" || s == "+inf" || s == "nan") return true;
+
+    size_t i = 0;
+    bool hasDot = false;
+    bool hasDigit = false;
+    if (s[i] == '+' || s[i] == '-') i++;
+    for (i < s.length(); i++){
+        if (isdigit(s[i])) {   
+            hasDigit = true;
+        } else if (s[i] == '.' && !hasDot) {
+            hasDot = true;
+        } else {
+            return false;
+        }
+    }
+    return hasDigit && hasDot;
 }
 
 Type dectecType(const std::string& str){
@@ -85,4 +122,11 @@ Type dectecType(const std::string& str){
 void ScalarConverte::convert(const std::string& str)
 {
     Type type = dectecType(str);
+    if (type == INVALID) {
+        std::cout << "char: impossible\nint: impossible\n"
+                  << "float: impossible\ndouble: impossible" << std::endl;
+        return;
+    }
+    
+
 }
